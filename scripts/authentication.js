81e9f05797aa3c325469beeb4c45f461cb516a58
Gurmehak Tuli <gurmehak1208@gmail.com>
2024-10-31 07:08:55 -0700

    Add login function using firebase

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,5 +1,6 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
@@ -32,10 +33,15 @@ var uiConfig = {
             }
             return false;
         },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<main.html>',
+    signInSuccessUrl: "main.html",
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -50,4 +56,5 @@ var uiConfig = {
     // Privacy policy url.
     privacyPolicyUrl: '<your-privacy-policy-url>'
 };
+
 ui.start('#firebaseui-auth-container', uiConfig);
