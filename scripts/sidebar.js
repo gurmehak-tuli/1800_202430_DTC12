bd7e0b9bf5a00178c05f44554b76f27db2873537
Gurmehak Tuli <gurmehak1208@gmail.com>
2024-11-27 15:41:40 -0800

    Fix sidebar

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,9 +1,14 @@
-const body = document.querySelector('body');
-      sidebar = body.querySelector('.sidebar');
-      toggle = body.querySelector('.toggle');
-      searchBtn = body.querySelector('.search-box');
-      modeText = body.querySelector('.mode-text');
+// const body = document.querySelector('body');
+//       sidebar = body.querySelector('.sidebar');
+//       toggle = body.querySelector('.toggle');
+//       searchBtn = body.querySelector('.search-box');
+//       modeText = body.querySelector('.mode-text');
 
-     toggle.addEventListener('click', () => {
-        sidebar.classList.toggle('close');
-     });
+//      toggle.addEventListener('click', () => {
+//         sidebar.classList.toggle('close');
+//      });
+
+function toggleSidebar() {
+   const sidebar = document.getElementById('sidebar');
+   sidebar.classList.toggle('active');
+}
